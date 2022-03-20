2675ac880f1d8815d71fd8757a084af5d97e764b
peterpeterparker <david.dalbusco@outlook.com>
2022-03-20 15:23:44 +0100

    feat: build.json that lists the resources

---
+++ build-kit.mjs
@@ -0,0 +1,30 @@
+#!/usr/bin/env node
+
+import fs from 'fs';
+import {join, extname} from 'path';
+import {writeFile} from 'fs/promises';
+
+const findEntryPoints = (dir, files) => {
+  fs.readdirSync(dir).forEach((file) => {
+    const fullPath = join(dir, file);
+    if (fs.lstatSync(fullPath).isDirectory()) {
+      findEntryPoints(fullPath, files);
+    } else {
+      files.push(fullPath);
+    }
+  });
+};
+
+const entryPoints = [];
+findEntryPoints('dist', entryPoints);
+
+const destPath = join(process.cwd(), 'dist', 'build.json');
+
+await writeFile(
+  destPath,
+  JSON.stringify(
+    entryPoints
+      .filter((entry) => !['.html', '.json'].includes(extname(entry)))
+      .map((entry) => entry.replace('dist/', ''))
+  )
+);
