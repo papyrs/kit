449609dc1ad0ec4b373fc4015a1b3fb1dbc01a86
peterpeterparker <david.dalbusco@outlook.com>
2022-09-16 13:27:09 +0200

    build: generate resources without hash

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,4 +5,15 @@ import svelte from "@astrojs/svelte";
 // https://astro.build/config
 export default defineConfig({
   integrations: [svelte()],
+  vite: {
+    build: {
+      rollupOptions: {
+        output: {
+          entryFileNames: `build/[name].js`,
+          chunkFileNames: `build/chunks/[name].js`,
+          assetFileNames: `assets/[name].[ext]`,
+        },
+      },
+    },
+  },
 });
