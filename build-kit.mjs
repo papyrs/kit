bfb125750ad957092dff0b58471705b694993c83
peterpeterparker <david.dalbusco@outlook.com>
2022-09-16 19:11:40 +0200

    build: generate sha256 for resources

--- build-kit.mjs
+++ build-kit.mjs
@@ -1,8 +1,9 @@
 #!/usr/bin/env node
 
-import fs from 'fs';
-import {join, extname} from 'path';
-import {writeFile} from 'fs/promises';
+import fs, { readFileSync } from "fs";
+import { join, extname } from "path";
+import { writeFile } from "fs/promises";
+import { createHash } from "crypto";
 
 export const findEntryPoints = (dir, files) => {
   fs.readdirSync(dir).forEach((file) => {
@@ -16,15 +17,24 @@ export const findEntryPoints = (dir, files) => {
 };
 
 const entryPoints = [];
-findEntryPoints('dist', entryPoints);
+findEntryPoints("dist", entryPoints);
 
-const destPath = join(process.cwd(), 'dist', 'build.json');
+const destPath = join(process.cwd(), "dist", "build.json");
+
+const toEntry = (entry) => {
+  const content = readFileSync(join(process.cwd(), entry), "utf-8");
+
+  return {
+    fullPath: entry.replace("dist/", ""),
+    sha2566: createHash("sha256").update(content).digest("base64"),
+  };
+};
 
 await writeFile(
   destPath,
   JSON.stringify(
     entryPoints
-      .filter((entry) => !['.html', '.json'].includes(extname(entry)))
-      .map((entry) => entry.replace('dist/', ''))
+      .filter((entry) => ![".html", ".json"].includes(extname(entry)))
+      .map(toEntry)
   )
 );
