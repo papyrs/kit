ea5aabadd4b9d6fd0393f68c4f0ec3794f8fc3ab
David Dal Busco <david.dalbusco@outlook.com>
2022-09-24 14:10:22 +0200

    feat: add signin

--- src/components/Toolbar.svelte
+++ src/components/Toolbar.svelte
@@ -1,21 +1,41 @@
 <script lang="ts">
-    import {Toolbar, IconChatBubbleOutline, IconThumbUp, IconShare} from "@papyrs/ui";
-    import { share } from "../services/share.services";
+  import {
+    Toolbar,
+    IconChatBubbleOutline,
+    IconThumbUp,
+    IconShare,
+    Modal,
+  } from "@papyrs/ui";
+  import { share } from "../services/share.services";
+
+  let open = false;
 </script>
 
 <Toolbar>
-    <button class="icon"><IconThumbUp /></button>
-    <button class="icon like"><IconChatBubbleOutline /></button>
-    <button class="icon" aria-label="Share" on:click={share}><IconShare /></button>
+  <button class="icon" on:click={() => (open = true)}><IconThumbUp /></button>
+  <button class="icon like"><IconChatBubbleOutline /></button>
+  <button class="icon" aria-label="Share" on:click={share}><IconShare /></button
+  >
 </Toolbar>
 
-<style lang="scss">
-    button {
-      height: 100%;
-      padding: 0.45rem 0.75rem;
-    }
+{#if open}
+  <Modal on:papyClose={() => (open = false)}>
+    <slot name="signin" />
+  </Modal>
+{/if}
 
-    .like {
-      padding-top: 0.85rem;
-    }
+<style lang="scss">
+  button {
+    height: 100%;
+    padding: 0.45rem 0.75rem;
+  }
+
+  .like {
+    padding-top: 0.85rem;
+  }
+
+  :global(div.modal) {
+    --modal-content-padding: 0 0 0 0.45rem;
+  }
+  
 </style>
