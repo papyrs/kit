53576e4649866b810cf93742ff15ff588e72bf52
David Dal Busco <david.dalbusco@outlook.com>
2022-09-27 21:38:42 +0200

    feat: like animation

--- src/components/Like.svelte
+++ src/components/Like.svelte
@@ -2,11 +2,13 @@
   import { IconThumbUp, Spinner } from "@papyrs/ui";
   import { createEventDispatcher } from "svelte";
   import { auth } from "../stores/auth.store";
+  import { fade, fly } from "svelte/transition";
 
   const dispatch = createEventDispatcher();
 
   let processing = false;
   let liked = false;
+  let likeAnimation: undefined | "+1" | "-1" = undefined;
 
   const onClick = () => {
     if (!$auth.loggedIn) {
@@ -17,26 +19,39 @@
     processing = true;
 
     setTimeout(() => {
-        liked = true;
-        processing = false;
-    }, 2000)
+      liked = true;
+      processing = false;
+      likeAnimation = "+1";
+
+      setTimeout(() => (likeAnimation = undefined), 1500);
+    }, 2000);
   };
 </script>
 
-<button
-  class="icon"
-  on:click={onClick}
-  aria-label="Like the article"
-  disabled={processing}
->
-  {#if processing}
-    <Spinner />
-  {:else}
-    <IconThumbUp fill={liked} />
+<div>
+  <button
+    class="icon"
+    on:click={onClick}
+    aria-label="Like the article"
+    disabled={processing}
+  >
+    {#if processing}
+      <Spinner />
+    {:else}
+      <IconThumbUp fill={liked} />
+    {/if}
+  </button>
+
+  {#if likeAnimation !== undefined}
+    <p in:fly={{ y: 10, duration: 1000 }} out:fade>{likeAnimation}</p>
   {/if}
-</button>
+</div>
 
 <style lang="scss">
+div {
+    position: relative;
+}
+
   button {
     position: relative;
     width: 100%;
@@ -44,5 +59,12 @@
     --spinner-size: 20px;
     --spinner-border-size: 2px;
     --spinner-margin: 0.05rem 0 0;
+  }
+
+  p {
+    position: absolute;
+    top: -30px;
+    left: 50%;
+    transform: translate(-50%, 0);
   }
 </style>
